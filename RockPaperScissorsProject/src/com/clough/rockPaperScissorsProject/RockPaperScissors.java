921e3b8633c05ae95ca8f3c88b621f6e5147c22e
jodia <jodia@DESKTOP-0TR476P>
2020-07-02 21:55:41 -0500

    Added error handling

--- RockPaperScissorsProject/src/com/clough/rockPaperScissorsProject/RockPaperScissors.java
+++ RockPaperScissorsProject/src/com/clough/rockPaperScissorsProject/RockPaperScissors.java
@@ -1,61 +1,29 @@
 package com.clough.rockPaperScissorsProject;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
-import java.util.function.BooleanSupplier;
 
 public class RockPaperScissors {
 
 	Scanner input = new Scanner(System.in);
 	Random rand = new Random();
 	
-	private int response;
-	private String choice;
-	private List<String> answers;
 	private int rock = 0;
 	private int paper = 1;
 	private int scissors = 2;
 	private String result;
-	private int answer;
-	
-	//private Array answers;
-	//answers = {0, 1, 2};
-	
-	public RockPaperScissors() {
-		answers = new ArrayList<String>();
-		answers.add("Rock");
-		answers.add("Paper");
-		answers.add("Scissors");
-	}
 
-	public int getResponse() {
-		boolean res;
+    public int getResponse() {
 		int response = input.nextInt();
-		//int response = 0;
-		//System.out.println(response);
-		/*if(response == 0){
-			System.out.println("True");
-			return true;
-			
-		}
-		else {
-			return false;
-		}*/
 		return response;
 		}
 
 	public int getAnswer() {
-		int answer = rand.nextInt(scissors);
-		//System.out.println(answer);
+		int answer = rand.nextInt(scissors + 1);
 		return answer;
 	}
 
 	public String getResult(int response, int answer) {
-		//int answer = 0;
-		//int response = 1;
 		if(answer==response) 
 			result = "It's a draw!!";
 					
--- RockPaperScissorsProject/src/com/clough/rockPaperScissorsProject/RockPaperScissorsDriver.java
+++ RockPaperScissorsProject/src/com/clough/rockPaperScissorsProject/RockPaperScissorsDriver.java
@@ -8,14 +8,22 @@ public class RockPaperScissorsDriver {
 		//int response = 0;
 		String choice = "";
 		//int answer;*/
+		int userinput;
+		int rock = 0;
+		int paper = 1;
+		int scissors = 2;
 		
 		RockPaperScissors ans = new RockPaperScissors();
 		
 		System.out.println("Welcome to the Ultimate Rock Paper Scissors Game!\n");
 		System.out.println("Enter one of the below responses to start the game:\n\n");
-		System.out.println("Rock: 0\n\nPaper: 1\n\nScissors: 2\n");
+		System.out.println("Rock: 0\n\nPaper: 1\n\nScissors: 2\n\n");
+		System.out.println("To exit: 5\n");
+		
+		try {
+		do {
 		System.out.print("Enter selection: ");
-		int userinput = ans.getResponse();
+		userinput = ans.getResponse();
 		
 		switch(userinput) {
 		case 0:
@@ -27,6 +35,15 @@ public class RockPaperScissorsDriver {
 		case 2:
 			choice = "Scissors";
 			break;
+		case 5:
+			choice = "Game over";
+			System.out.println(choice);
+			System.exit(0);
+		default:
+			System.out.println("Invalid entry please try again");
+			System.exit(0);
+			
+			
 		}
 		System.out.println("You chose " + choice);
 
@@ -49,7 +66,13 @@ public class RockPaperScissorsDriver {
 	
 	ans.getResult(userinput, sysoutput);
 	
+		}while(userinput != 5);
+	
 
 }
+		catch(Exception e) {
+			System.out.println("Invalid entry please try again");
+			System.exit(0);
+		}
 	
-}
+}}
